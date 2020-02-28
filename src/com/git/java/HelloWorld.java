db609ffb6764fa9960bc8f8ff46fd02d77a1352f
wz <531970615@qq.com>
2020-02-28 21:48:02 +0800

    My first commit

---
+++ src/com/git/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.git.java;
+
+public class HelloWorld {
+		public static void main(String[] args)
+		{
+			System.out.println("Hello");
+			System.out.println();
+		
+		}
+		public void name() {
+			
+		}
+}
