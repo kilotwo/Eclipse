3fff6a8943dcc6d187110fb006e5b13a835505ad
C.oder <531970615@qq.com>
2020-02-28 22:29:24 +0800

    Update Apple.java

--- src/com/git/java/Apple.java
+++ src/com/git/java/Apple.java
@@ -3,5 +3,7 @@ package com.git.java;
 public class Apple {
 		public static void main(String[] args) {
 			System.out.println("aaaaa");
+			System.out.println("aaaaa");
+			System.out.println("aaaaa");
 		}
 }
