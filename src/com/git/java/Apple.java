886769f0465f6778a4ab7700ed8e2872ac347daf
wz <531970615@qq.com>
2020-02-28 22:30:42 +0800

    bbbbb

--- src/com/git/java/Apple.java
+++ src/com/git/java/Apple.java
@@ -4,6 +4,6 @@ public class Apple {
 		public static void main(String[] args) {
 			System.out.println("aaaaa");
 			System.out.println("aaaaa");
-			System.out.println("aaaaa");
+			System.out.println("bbbbbb");
 		}
 }
